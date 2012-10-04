4e1a020d683b4c57142ce3279976e856573bcf58
David Gasaway <ponsasinorem@gmail.com>
2012-10-03 18:58:22 -1000

    Compile the kernel
    Change-Id: I1a50e88482387d81e96e9de2fb65e754350037b6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,9 +31,7 @@ USE_CAMERA_STUB := false
 include device/htc/qsd8k-common/BoardConfigCommon.mk
 
 # Override qsd8k-common/BoardConfigCommon.mk to use incredikernel
-#TARGET_KERNEL_SOURCE := kernel/htc/incredikernel
-# Use the precompiled kernel
-TARGET_KERNEL_SOURCE :=
+TARGET_KERNEL_SOURCE := kernel/htc/incredikernel
 # Override kernel toolchain. (4.6 is too unstable)
 ifeq ($(LINARO_BUILD),)
 KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
