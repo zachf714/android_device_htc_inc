664668bbf0f2b5b59db8632ec7bf556f6edab768
David Gasaway <ponsasinorem@gmail.com>
2012-10-27 11:00:35 -1000

    post_boot: remove unused mfreq

--- prebuilt/etc/init.qcom.post_boot.sh
+++ prebuilt/etc/init.qcom.post_boot.sh
@@ -20,10 +20,6 @@ case "$target" in
         echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
         echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
         echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-        # /system/lib/libqct-opt JNI native will write
-        # mfreq to set CPU freq to max for performance
-        chown root.system /sys/devices/system/cpu/mfreq
-        chmod 220 /sys/devices/system/cpu/mfreq
         # Configure and enable KSM
         echo 1000 > /sys/kernel/mm/ksm/sleep_millisecs
         echo 100 > /sys/kernel/mm/ksm/pages_to_scan
