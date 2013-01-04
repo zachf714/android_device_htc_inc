a3e60cb15ad55a32e6ff16a2f5094972d63c4640
Andrew Bartholomew <andrewb03@gmail.com>
2013-01-04 09:12:35 -0500

    inc: Build kernel with Linaro 4.7 toolchain
    Change-Id: I207f8909bc915c627ea3c395184f6ff00eb0e30f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,9 @@ include device/htc/qsd8k-common/BoardConfigCommon.mk
 # Override qsd8k-common/BoardConfigCommon.mk to use incredikernel
 TARGET_KERNEL_SOURCE := kernel/htc/incredikernel
 # Override kernel toolchain. (4.6 is too unstable)
-KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+#KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+# Use Linaro 4.7 toolchain
+KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/linaro-toolchain/bin/arm-eabi-
 
 TARGET_BOOTLOADER_BOARD_NAME := inc
 
