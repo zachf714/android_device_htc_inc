9ee1058dfeeee555988d16ccca0fede625cc5fe5
Zach <zachf714@gmail.com>
2014-01-06 22:10:50 -0500

    Full telephony =P

--- full_inc.mk
+++ full_inc.mk
@@ -113,10 +113,6 @@ $(call inherit-product, device/htc/qsd8k-common/qsd8k.mk)
 
 $(call inherit-product-if-exists, vendor/htc/inc/inc-vendor.mk)
 
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_EVERVOLV_DIR)/product/lite_base_telephony.mk)
-
-
 PRODUCT_NAME := full_inc
 PRODUCT_DEVICE := inc
-PRODUCT_MODEL := Full Android on Inc
+PRODUCT_MODEL := Droid Incredible
