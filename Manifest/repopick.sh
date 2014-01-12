2b0ba127598f9e8ecfc8385ab9ed187d99b156e4
Zach <zachf714@gmail.com>
2014-01-11 22:53:51 -0500

    Update build script
    Change-Id: Ic2804e8df322036107ffaac850908c93af7bb11a

--- Manifest/repopick.sh
+++ Manifest/repopick.sh
@@ -11,6 +11,7 @@ repopick 56080  # Multi-window ported from omnirom
 cd ~/android/INC-CM-11/frameworks/opt/telephony || exit 1
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/58/9758/1 && git cherry-pick FETCH_HEAD
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/59/9759/1 && git cherry-pick FETCH_HEAD
+cd ~/android/INC-CM-11/external/iproute2 || exit 1 && git revert 4c48963
 cd ~/android/INC-CM-11 || exit 1 && . build/envsetup.sh 
 
 read -p Done
