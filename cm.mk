17af2f3e4f2ff842d171ea37b56d0bc4617cbcd7
Zach <zachf714@gmail.com>
2014-01-12 20:52:31 -0500

    Change Build Name
    Change-Id: Ib2dd9a93ce8444fec9917e06c26981df03b68dd9

--- cm.mk
+++ cm.mk
@@ -14,7 +14,7 @@ TARGET_SCREEN_WIDTH := 480
 # Inherit device configuration
 $(call inherit-product, device/htc/inc/full_inc.mk)
 
-TARGET_UNOFFICIAL_BUILD_ID := HAIL-TO-THE-KING
+TARGET_UNOFFICIAL_BUILD_ID := ADTR
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := inc
