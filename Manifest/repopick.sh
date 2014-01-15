05adeb920642f35e3a8d6987832f3908482e785e
Zach <zachf714@gmail.com>
2014-01-15 13:41:15 -0500

    Update RepoPicks
    Change-Id: I41cab554faa6facc9e8ff5704e37b7fe9110c044

--- Manifest/repopick.sh
+++ Manifest/repopick.sh
@@ -7,11 +7,14 @@ cd ~/android/INC-CM-11/ && . build/envsetup.sh ||exit 1
 repopick 55041  # Settings: Add CM PlatLogo & CMCaseView (1/2)
 repopick 55037  # CM PlatLogo: CyanogenMod version preference (2/2)
 repopick 56080  # Multi-window ported from omnirom
+repopick 57536  # DCTracker: call setupAllAPN when the dctracker constructed
 
 cd ~/android/INC-CM-11/frameworks/opt/telephony || exit 1
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/58/9758/1 && git cherry-pick FETCH_HEAD
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/59/9759/1 && git cherry-pick FETCH_HEAD
 cd ~/android/INC-CM-11/external/iproute2 || exit 1 && git revert 4c48963
+cd ~/android/INC-CM-11/build || exit 1
+git fetch https://gerrit.omnirom.org/android_build refs/changes/37/4537/2 && git cherry-pick FETCH_HEAD
 cd ~/android/INC-CM-11 || exit 1 && . build/envsetup.sh 
 
 read -p Done
