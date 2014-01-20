c067b6d90d0dc74e0eb745676a81a07621a15db1
Zach <zachf714@gmail.com>
2014-01-20 17:05:20 -0500

    Update pick script
    Change-Id: Ia8f5a3682fdc2dcba2502a11af3ccdbb57ef861d

--- Manifest/repopick.sh
+++ Manifest/repopick.sh
@@ -8,6 +8,11 @@ repopick 55041  # Settings: Add CM PlatLogo & CMCaseView (1/2)
 repopick 55037  # CM PlatLogo: CyanogenMod version preference (2/2)
 repopick 56080  # Multi-window ported from omnirom
 repopick 57536  # DCTracker: call setupAllAPN when the dctracker constructed
+repopick 56515  # vold: allow swapped storage paths at vold init
+repopick 56516  # Advanced storage: vold swap storage paths
+repopick 56950  # storage: allow swapping primary storage for apps
+repopick 56951  # Advanced storage: swap primary storage for apps
+
 
 cd ~/android/INC-CM-11/frameworks/opt/telephony || exit 1
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/58/9758/1 && git cherry-pick FETCH_HEAD
