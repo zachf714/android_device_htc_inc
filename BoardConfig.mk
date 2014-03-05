6384397c9e4a67f1975fbc1d164d4f99424bf2b6
Zach <zachf714@gmail.com>
2014-03-05 17:49:57 -0500

    Switch back to the evervolv kernel
    For now, while I have this tree building pac I dont want to
    use an "experimental" kernel with the build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x09500000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Try to build the kernel
-TARGET_KERNEL_CONFIG    := incrediblec-incredikernel_defconfig
+TARGET_KERNEL_CONFIG    := evervolv_incrediblec_defconfig
 # Keep this as a fall back in case of no source
 #TARGET_PREBUILT_KERNEL  := device/htc/inc/prebuilt/root/kernel
 TARGET_RECOVERY_FSTAB   := device/htc/inc/prebuilt/root/fstab.inc
