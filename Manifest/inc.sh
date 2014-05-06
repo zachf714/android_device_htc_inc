4e0491b3874fc7d166b9f7716239b17d8fc42173
Zach <zachf714@gmail.com>
2014-05-05 23:03:04 -0400

    inc.sh: Updates to picks

--- Manifest/inc.sh
+++ Manifest/inc.sh
@@ -10,12 +10,12 @@ cd ~/android/Dinc-AOKP/ && . build/envsetup.sh ||exit 1
 
 cd ~/android/Dinc-AOKP/frameworks/base || exit 1
 pstest 16063  # Lockscreen Notifications [1/2]
-pstest 16316  # Change video recording toggle drawable in Quick Settings
 pstest 16660  # base: Fix screen on stats after a boot
 pstest 16674  # framework-res: Remove gingerbread scrolls
-pstest 16659  # frameworks/base: Fix the random UI corruption
 cd ~/android/Dinc-AOKP/packages/apps/ROMControl || exit 1
 pstest 16213  # Lockscreen Notifications [2/2]
+cd ~/android/Dinc-AOKP/packages/apps/InCallUI ||exit 1
+pstest 16804  # Fullscreen UI when in Incoming call based on CM
 cd ~/android/Dinc-AOKP/frameworks/opt/telephony || exit 1
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/58/9758/1 && git cherry-pick FETCH_HEAD
 git fetch http://review.evervolv.com/android_frameworks_opt_telephony refs/changes/59/9759/1 && git cherry-pick FETCH_HEAD
